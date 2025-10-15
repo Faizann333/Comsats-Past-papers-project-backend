444899f01f5beb3c2776434d2c085f874f9fddd9
Faizan262 <mrfaizanmughal333@gmail.com>
2025-10-15 23:22:13 +0500

    solved corse issue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 dotenv.config();
 
 //external modules
-const {routes}  = require('./Routes/index');
+const { routes } = require('./Routes/index');
 
 const app = express();
 
@@ -15,14 +15,15 @@ app.use(express.json());
 const allowedOrigins = [
   'https://pastpaperportal.vercel.app'
 ];
-app.use(cors({
-  origin: allowedOrigins,  // your frontend
-  credentials: true,       // allow cookies
-}));
 
-// Handle preflight OPTIONS requests for all routes
-app.options('/*', cors({
-  origin: allowedOrigins,
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 }));
 
@@ -31,11 +32,11 @@ app.use(cookieParser());
 routes(app);
 
 const PORT = process.env.PORT;
-mongoose.connect(process.env.MONGO_DB_URL).then(()=>{
+mongoose.connect(process.env.MONGO_DB_URL).then(() => {
   console.log("Connected to MongoDB successfully");
   app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
-  })
-}).catch((err)=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  });
+}).catch((err) => {
   console.error("Error connecting to MongoDB", err);
-})
+});
