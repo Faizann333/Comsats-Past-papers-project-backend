8d2140cd8008c7010b33530bcf40c430c706295e
Faizan262 <mrfaizanmughal333@gmail.com>
2025-10-16 23:33:49 +0500

    solved auth/me error

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -111,7 +111,7 @@ exports.postLoginController = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "Strict",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         // console.log("Setting cookie with token:", token);
