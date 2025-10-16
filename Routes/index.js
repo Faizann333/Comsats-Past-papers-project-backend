1e0c82ee750f5d70e3b4c6dd2396a5a8e1165a7b
Faizan262 <mrfaizanmughal333@gmail.com>
2025-10-16 22:46:15 +0500

    path error solve

--- Routes/index.js
+++ Routes/index.js
@@ -4,8 +4,8 @@ const adminRoute = require('./adminRoute')
 
 
 function routes(app) {
-    app.use('/api/user',userRoute);
-    app.use("/api/auth", authRoute);
+    app.use('/user',userRoute);
+    app.use("/auth", authRoute);
     app.use('/api/admin',adminRoute)
 }
 module.exports = {routes};
